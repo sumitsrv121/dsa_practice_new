ba9f63ab59420c442b72df1b8f08a79266e06957
sumit <sumitsaurav121@gmail.com>
2025-06-07 16:06:33 +0530

    Added code for count odd digit

--- src/main/java/basic_math/CountNumberOfOddDigitsInANumber.java
+++ src/main/java/basic_math/CountNumberOfOddDigitsInANumber.java
@@ -2,6 +2,20 @@ package basic_math;
 
 public class CountNumberOfOddDigitsInANumber {
     public static void main(String[] args) {
+        CountNumberOfOddDigitsInANumber countNumberOfOddDigitsInANumber = new
+                CountNumberOfOddDigitsInANumber();
 
+        System.out.println(countNumberOfOddDigitsInANumber.countOddDigit(15));
+    }
+
+    public int countOddDigit(int n) {
+        int count = 0;
+        while (n > 0) {
+            if ((n % 10) % 2 != 0) {
+                count++;
+            }
+            n /= 10;
+        }
+        return count;
     }
 }
