709bcf8782aa563f0c7708fd7a926e6219d03d93
sumit <sumitsaurav121@gmail.com>
2025-06-07 16:48:05 +0530

    Added code to check if number is Palindrome

---
+++ src/main/java/basic_math/PalindromeNumber.java
@@ -0,0 +1,21 @@
+package basic_math;
+
+public class PalindromeNumber {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean isPalindrome(int n) {
+        String num = String.valueOf(n);
+
+        int start = 0;
+        int end = num.length() - 1;
+
+        while (start < end) {
+            if (num.charAt(start++) != num.charAt(end--)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
