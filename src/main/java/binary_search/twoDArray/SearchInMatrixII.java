7d6577996e43e9eab120ce54190c7c9c4e3a625c
sumit <sumitsaurav121@gmail.com>
2025-07-13 08:40:52 +0530

    Added solution to search in sorted matrix pattern II

---
+++ src/main/java/binary_search/twoDArray/SearchInMatrixII.java
@@ -0,0 +1,31 @@
+package binary_search.twoDArray;
+
+public class SearchInMatrixII {
+    public static void main(String[] args) {
+        SearchInMatrixII search = new SearchInMatrixII();
+
+        System.out.println(search.searchMatrix(new int[][]{{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19}, {3, 6, 9, 16, 22}, {10, 13, 14, 17, 24}, {18, 21, 23, 26, 30}}, 5));
+        System.out.println(search.searchMatrix(new int[][]{{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19}, {3, 6, 9, 16, 22}, {10, 13, 14, 17, 24}, {18, 21, 23, 26, 30}}, 20));
+        System.out.println(search.searchMatrix(new int[][]{{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19}, {3, 6, 9, 16, 22}, {10, 13, 14, 17, 24}, {18, 21, 23, 26, 30}}, 1));
+
+    }
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        int row = 0;
+        int col = m - 1;
+
+        while (row < n && col >= 0) {
+            if (matrix[row][col] == target) {
+                return true;
+            } else if (matrix[row][col] > target) {
+                col--;
+            } else {
+                row++;
+            }
+        }
+        return false;
+    }
+}
